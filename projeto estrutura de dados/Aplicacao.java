873cf0b16da950fff56ebe6af75f607e112709c7
Guilherme Shinohara <guiba.shinohara@gmail.com>
2025-03-25 20:13:21 -0300

    Update Aplicacao.java
    adicionando menu de selecao

--- projeto estrutura de dados/Aplicacao.java
+++ projeto estrutura de dados/Aplicacao.java
@@ -3,6 +3,38 @@ import java.util.*;
 public class Aplicacao{
     
     public static void main(String[] args){
+        Scanner sc = new Scanner();
+	    System.out.println("SISTEMA DE LOCAÇÃO DE BICICLETAS"+
+	                       "1 - CADASTRAR NOVO CLIENTE"+
+                           "2 - LOCAR BICICLETA"+
+                           "3 - DEVOLVER BICICLETA"+
+                           "4 - REMOVER CLIENTE"+
+                           "5 - EXIBIR DADOS DO CLIENTE"+
+                           "6 - LISTAR CLIENTES EM ORDEM DECRESCENTE DE GASTOS"+
+                           "7 - EXIBIR CLIENTE MAIS RENTAVEL"+
+                           "8 - ENCERRAR");
+	    int a = sc.nextInt(System.in):
+	    
+	    switch(a){
+	        case 1 : ;
+	        
+	        case 2 : ;
+	        
+	        case 3 : ;
+	        
+	        case 4 : ;
+	        
+	        case 5 : ;
+	        
+	        case 6 : ;
+	        
+	        case 7 : ;
+	        
+	        case 8 : ;
+	        
+	        default : System.out.println("Opção inválida");
+        }
+        
         Cliente [] clientes = new Cliente [10];
 
         
