d15c82b438c79d9b579ff2ce3388f6495f19011c
KauanSarzi <kauansarzi57@gmail.com>
2025-04-22 12:45:40 -0300

    Update App.java

--- Lista Encadeada/No/src/App.java
+++ Lista Encadeada/No/src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static void main(String[] args) {
         ListaEncadeada lista = new ListaEncadeada();
 
-        lista.inserir(10);
+        lista.inserirInicio(10);
         lista.inserirNoFim(20);
         lista.inserirNoFim(30);
-        lista.inserir(5);
+        lista.inserirInicio(5);
 
         lista.imprimir(); // 5 -> 10 -> 20 -> 30 -> null
 
